7f69183878498b9bf97882aa504f0dcf6138d40b
Gianni Settino <g@miltonandparc.com>
2014-08-17 19:27:07 +0200

    Minor

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express()
 var foursquare = require('node-foursquare-venues')('35UH1ZRV1LML4OMHUNV2CISGII0GHFILV3Z1CHDBQB5WHIO1', '5NCVL2KJG5WDNS4KX2ZR5SVP3UDMFJNZL04LSHJLLDL5ZY0G')
 
 app.get('/', function(req, res){
+	res.send('#nonnaknowseverything');
+});
+
+app.get('/knows', function(req, res){
 	var params = { 'll' : req.query.lat+','+req.query.lng}
 	foursquare.venues.explore(params, function(err, data) {
 		if (!err) {
